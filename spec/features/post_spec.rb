f2451a4c065a0527c938e20e85e5fa2afa74b67b
Filipe Gorges Reuwsaat <Filipe Gorges Reuwsaat>
2017-03-14 00:43:44 -0300

    Implemented tests and page redirection for permission structure

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -37,11 +37,10 @@ describe 'navigate' do
   end
 
   describe 'delete' do
-    before do
+    it 'can be deleted' do
       @post = FactoryGirl.create(:post)
       visit posts_path
-    end
-    it 'can be deleted' do
+
       click_link("delete_post_#{@post.id}_from_index")
       expect(page.status_code).to eq(200)
     end
@@ -59,7 +58,6 @@ describe 'navigate' do
     it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Some rationale'
-
       click_on 'Save'
 
       expect(page).to have_content('Some rationale')
@@ -76,18 +74,13 @@ describe 'navigate' do
 
   describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
-    end
-
-    it 'can be reached by clicking edit on index page' do
-      visit posts_path
-
-      click_link "edit_#{@post.id}"
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: 'asdf', last_name: 'asdf', email: 'asdfasdf@asdf.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
+      login_as(@edit_user, scope: :user)
+      @edit_post = Post.create(date: Date.today, rationale: 'asdf', user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: 'Edited content'
@@ -95,6 +88,15 @@ describe 'navigate' do
 
       expect(page).to have_content("Edited content")
     end
-  end
 
+    it "cannot be edited by a non authorized user" do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, scope: :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
+    end
+  end
 end
